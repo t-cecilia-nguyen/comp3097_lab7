d2774b13db8644bc7a6b3dc2efc87e2a9e036d87
Trang Nguyen <w5749684@macmini03.gbccloud.com>
2025-02-18 16:02:10 -0500

    lab 7

---
+++ Demo3/ViewController.swift
@@ -0,0 +1,58 @@
+//
+//  ViewController.swift
+//  Lab7
+//
+//  Created by Trang Nguyen on 2025-02-18.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+
+    @IBOutlet weak var calculateButton: UIButton!
+    @IBOutlet weak var numberTextField: UITextField!
+    @IBOutlet weak var tableView: UITableView!
+       
+    var timesTable: [String] = []
+    var foods: [String]?
+    var unhealthyFoods: [String]?
+    var healthyFoods: [String]?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        numberTextField.keyboardType = .numberPad
+        
+        foods = ["Apple", "Banana", "Burger", "Fries", "Orange", "Pizza"]
+        unhealthyFoods = ["Burger", "Fries", "Pizza"]
+        healthyFoods = ["Apple", "Banana", "Orange"]
+    }
+    
+    
+    @IBAction func generateTimesTable(_ sender: UIButton) {
+        guard let inputText = numberTextField.text, let number = Int(inputText) else {
+            return
+        }
+        
+        timesTable = (1...10).map {
+            "\($0) x \(number) = \($0 * number)"
+        }
+        
+        tableView.reloadData()
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return timesTable.count
+    }
+    
+    func tableView (_ tabelView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: .default, reuseIdentifier: "")
+        let timetableEntry = timesTable[indexPath.row]
+        cell.textLabel?.text = timetableEntry
+        return cell
+    }
+}
+
